39fd5043e678c3b8f40541a8d7ba6f0a2e501640
Asirt <45468633+Asirt@users.noreply.github.com>
2019-03-01 22:08:44 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,35 +1,52 @@
-function _toConsumableArray(arr) {if (Array.isArray(arr)) {for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {arr2[i] = arr[i];}return arr2;} else {return Array.from(arr);}}document.querySelector('a').innerHTML = '<span>' + document.querySelector('a').innerHTML.split('').join('</span><span>') + '</span>';
+function _toConsumableArray(arr) {
+  if (Array.isArray(arr)) {
+    for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {
+      arr2[i] = arr[i];
+    }
+    return arr2;
+  } else {
+    return Array.from(arr);
+  }
+}
+document.querySelector('.fancy a').innerHTML = '<span>' + document.querySelector('.fancy a').innerHTML.split('').join('</span><span>') + '</span>';
 
-[].concat(_toConsumableArray(document.querySelectorAll('span'))).forEach(function ($e) {
+[].concat(_toConsumableArray(document.querySelectorAll('span'))).forEach(function($e) {
   $e.innerHTML = $e.innerHTML + ('<i>' + $e.innerHTML + '</i>').repeat(7);
 });
 
 $(window).scroll(function() {
-  
+
   // selectors
   var $window = $(window),
-      $body = $('body'),
-      $panel = $('.panel');
-  
+    $body = $('body'),
+    $panel = $('.panel');
+
   // Change 33% earlier than scroll position so colour is there when you arrive.
   var scroll = $window.scrollTop() + ($window.height() / 3);
- 
-  $panel.each(function () {
+
+  $panel.each(function() {
     var $this = $(this);
-    
+
     // if position is within range of this panel.
     // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
     // Remember we set the scroll to 33% earlier in scroll var.
     if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
-          
+
       // Remove all classes on body with color-
-      $body.removeClass(function (index, css) {
-        return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+      $body.removeClass(function(index, css) {
+        return (css.match(/(^|\s)color-\S+/g) || []).join(' ');
       });
-       
+
       // Add class of currently active div
       $body.addClass('color-' + $(this).data('color'));
     }
-  });    
-  
+  });
+
 }).scroll();
+
+function ScrollClick(name) {
+  var elmnt = document.getElementById(name);
+  elmnt.scrollIntoView({
+    behavior: 'smooth'
+  });
+};
