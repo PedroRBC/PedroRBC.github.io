3bacd96b002dff0a555d1e8808c171ff433e5d53
Asirt <45468633+Asirt@users.noreply.github.com>
2019-03-04 21:11:35 +0100

    Add files via upload

--- js/indexScroll.js
+++ js/indexScroll.js
@@ -3,7 +3,7 @@ $(window).scroll(function() {
   // selectors
   var $window = $(window),
     $body = $('body'),
-    $panel = $('.panel');
+    $panel = $('section');
 
   // Change 33% earlier than scroll position so colour is there when you arrive.
   var scroll = $window.scrollTop() + ($window.height() / 3);
@@ -16,13 +16,9 @@ $(window).scroll(function() {
     // Remember we set the scroll to 33% earlier in scroll var.
     if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
 
-      // Remove all classes on body with color-
-      $body.removeClass(function(index, css) {
-        return (css.match(/(^|\s)color-\S+/g) || []).join(' ');
-      });
-
-      // Add class of currently active div
       $body.addClass('color-' + $(this).data('color'));
+    } else {
+      $body.removeClass('color-' + $(this).data('color'));
     }
   });
 
